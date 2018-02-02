a02fa0a51c7848cbd620bfe3820a77984626c14e
ds3068 <36058058+ds3068@users.noreply.github.com>
2018-02-01 17:30:36 -0800

    close the compilation window if no lint error

--- misc/emacs/golint.el
+++ misc/emacs/golint.el
@@ -13,6 +13,9 @@
 ;;   (require 'golint)
 ;;
 ;; After this, type M-x golint on Go source code.
+;; You can also auto invoke golint after save by adding the
+;; following into .emacs file:
+;;   (add-hook 'after-save-hook 'golint)
 ;;
 ;; Usage:
 ;;   C-x `
@@ -31,22 +34,45 @@
   "Setup compilation variables and buffer for `golint'."
   (run-hooks 'golint-setup-hook))
 
+(defun close-if-no-error (compilation-buffer process-result)
+  "close the golint buffer if no error was found"
+  (interactive)
+  (when (and
+         (string-match "*golint*" (buffer-name compilation-buffer))
+         (string-match "finished" process-result))
+    (setq err_count 0)
+    (condition-case nil
+        (while t
+          (next-error)
+          (setq err_count (+ err_count 1))
+          )
+      (error nil))
+    (when (eq err_count 0)
+      (kill-buffer "*golint*")
+      )
+    )
+  )
+
 (define-compilation-mode golint-mode "golint"
   "Golint is a linter for Go source code."
   (set (make-local-variable 'compilation-scroll-output) nil)
   (set (make-local-variable 'compilation-disable-input) t)
   (set (make-local-variable 'compilation-process-setup-function)
        'golint-process-setup)
+  (set (make-local-variable 'compilation-finish-functions)
+       'close-if-no-error)
+
 )
 
 ;;;###autoload
 (defun golint ()
   "Run golint on the current file and populate the fix list. Pressing C-x ` will jump directly to the line in your code which caused the first message."
   (interactive)
-  (compilation-start
-   (mapconcat #'shell-quote-argument
-              (list "golint" (expand-file-name buffer-file-name)) " ")
-   'golint-mode))
+  (if (eq major-mode 'go-mode)
+      (compilation-start
+       (mapconcat #'shell-quote-argument
+                  (list "golint" (expand-file-name buffer-file-name)) " ")
+       'golint-mode)))
 
 (provide 'golint)
 
